a8fc5ddb7603311b1e840219df21fb86f6470f9e
nidhi <nidhi7042sharma@gmail.com>
2024-03-06 10:42:09 +0530

    changed icon

--- frontend/src/components/feature/SingleFeatureV3.jsx
+++ frontend/src/components/feature/SingleFeatureV3.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Fade } from 'react-reveal';
 import { HashLink as Link } from 'react-router-hash-link';
+import iconWork from "../../assets/images/features/Icon_Work.svg"
+import iconStudy from "../../assets/images/features/Icon_Study.svg"
+import iconpartner from "../../assets/images/features/Icon_Partner.svg"
 
+const imageSource={
+    "Icon_Work.svg":iconWork,
+    "Icon_Study.svg":iconStudy,
+    "Icon_Partner.svg":iconpartner
+}
 const SingleFeatureV3 = ({ feature }) => {
     const { thumb, title, text, delay } = feature
-
+    let imgSrc=imageSource[thumb]
     return (
         <>
             <Fade className="fadeInUp" delay={delay}>
                 <div className="feature-item-three">
                     <div className="icon">
-                        <img src={`/images/feature/${thumb}`} alt="Icon" />
+                        <img src={imgSrc} alt="Icon" />
                     </div>
                     <div className="feature-content">
                         <h3><Link to="/service-details#">{title}</Link></h3>
